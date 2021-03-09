f64b0702a40863a9d49199468045e4c4a53864a3
Luca D'Amico <damico.luca91@live.it>
2021-03-09 22:23:55 +0100

    Force Fusion theme - fixes macOS UI bug

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "ui/mainwindow.h"
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
